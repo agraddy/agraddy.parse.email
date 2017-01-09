bb61837b1906d175318d300a509f916e80df0b46
Anthony Graddy <anthony@locationq.com>
2017-01-09 05:13:35 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,40 @@
 var tap = require('agraddy.test.tap')(__filename);
+var fs = require('fs');
+var path = require('path');
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+var actual;
+var expected;
 
+process.chdir('test');
+
+// Gmail
+actual = mod(getEmail('gmail.eml'));
+expected = getJSON('gmail.json');
+tap.assert.deepEqual(actual, expected, 'Should be equal.');
+
+// Gmail With Attachment
+actual = mod(getEmail('gmail_attachment.eml'));
+expected = getJSON('gmail_attachment.json');
+console.log('actual');
+console.log(actual);
+console.log('expected');
+console.log(expected);
+tap.assert.deepEqual(actual, expected, 'Should be equal.');
+
+
+
+
+
+// Need to setup a test where multiple colons appear in a header (like the subject)
+
+
+function getEmail(input) {
+	return fs.readFileSync(path.join('fixtures', input)).toString();
+}
+
+function getJSON(input) {
+	return require('./fixtures/' + input);
+}
 
